143c678815896ba21866937f76d0bf9481027758
HakanKayaPTTSeniorDeveloper <133164728+HakanKayaPTTSeniorDeveloper@users.noreply.github.com>
2023-06-01 14:04:01 +0300

    Refactor

---
+++ Core/Utilities/Interceptors/MethodInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    public class MethodInterceptor : MethodInterceptorBaseAttribute
+    {
+        protected virtual void OnBefore (IInvocation invocation){}
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+        protected void OnException(IInvocation invocation,Exception ex) { }
+        public override void Intercept(IInvocation invocation)
+        {
+            OnBefore(invocation);
+            bool success = true;
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex )
+            {
+                success = false;
+                OnException(invocation, ex);    
+                throw;
+            }
+            finally
+            {
+                if(success)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
